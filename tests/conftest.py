9e8dd30005da496df3e57f10d5301941f37c16ec
LKRenjoyer <m.ovczarz@gmail.com>
2026-03-20 17:22:38 +0100

    Add, testing for InkData object

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,13 @@ import pytest
 @pytest.fixture
 def fixtures_dir() -> Path:
     return Path(__file__).parent / 'fixtures'
+
+
+@pytest.fixture
+def sample_inkml(fixtures_dir: Path) -> Path:
+    return fixtures_dir / 'sample.inkml'
+
+
+@pytest.fixture
+def minimal_symbols_inkml(fixtures_dir: Path) -> Path:
+    return fixtures_dir / 'minimal_symbols.inkml'
