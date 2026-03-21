c8431d84d6dead4479ae624646263c9785f2c62a
LKRenjoyer <m.ovczarz@gmail.com>
2026-03-21 22:10:52 +0100

    Add, tests for LatexVocab object

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import pytest
 
+from hand_to_tex.utils import LatexVocab
+
 
 @pytest.fixture
 def fixtures_dir() -> Path:
@@ -15,3 +17,8 @@ def sample_inkml(fixtures_dir: Path) -> Path:
 @pytest.fixture
 def minimal_symbols_inkml(fixtures_dir: Path) -> Path:
     return fixtures_dir / 'minimal_symbols.inkml'
+
+
+@pytest.fixture
+def vocab() -> LatexVocab:
+    return LatexVocab.default()
