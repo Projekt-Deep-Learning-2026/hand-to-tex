bdeab38d50a8f4428737252be361ecddd8f4cf3b
LKRenjoyer <m.ovczarz@gmail.com>
2026-03-23 00:23:11 +0100

    Add, first steps for feature extraction of ink traces

--- src/hand_to_tex/datasets/dataset.py
+++ src/hand_to_tex/datasets/dataset.py
@@ -2,9 +2,9 @@ from pathlib import Path
 import torch
 from torch.utils.data.dataset import Dataset
 from typing import Optional, Callable
-import matplotlib.pyplot as plt
+import numpy as np
 
-from .ink_data import InkData
+from .ink_data import InkData, Trace
 from ..utils import LatexVocab
 
 
@@ -12,11 +12,13 @@ class HMEDataset(Dataset):
     """Handwritten Mathematical Expressions dataset for processing
     `.inkml` files
     """
+    EPS: float = 1e-8
+
     def __init__(
                 self,
                 root: Path | str,
                 vocab: Optional[LatexVocab] = None,
-                transform: Optional[Callable] = None
+                transform: Optional[Callable] = None,
             ):
         """Creates the dataset
 
@@ -40,11 +42,72 @@ class HMEDataset(Dataset):
 
     def __getitem__(self, idx: int):
         # p = self.filenames[idx]
+        return InkData.load(self.filenames[idx])
+        # raise NotImplementedError
 
-        raise NotImplementedError
+    @staticmethod
+    def extract_features(ink: InkData) -> torch.Tensor:
+        """Extracts features of an InkData object
+
+        Returns
+        -------
+
+        torch.Tensor containing following features:
+            - x_norm, y_norm : float 
+                Normalised x,y coordinates of a TracePoint
+            - dx, dy, dt : float
+                V
+
+        """
+        data = HMEDataset._flatten_traces(ink.traces)
+        data_norm = HMEDataset._normalise_data(data)
+        # X, Y, T, TraceID = [], [], [], []
+        # for t_id, trace in enumerate(ink.traces):
+        #     for x, y, t in trace:
+        #         X.append(x)
+        #         Y.append(y)
+        #         T.append(t)
+        #         TraceID.append(t_id)
+        # X = np.array((x for x,_,_ in trace) for trace in ink.traces)
+        pass
+
+    @staticmethod
+    def _flatten_traces(traces: list[Trace]) -> torch.Tensor:
+        """Returns a tensor of elements (x, y, t, trace_id) for InkData traces
+
+        Parameters
+        ----------
+
+        traces : Traces
+            List of Trace elements obtained from `InkData.traces`
+        """
+        result = []
+
+        for t_id, t in enumerate(traces):
+            points = torch.tensor(t, dtype=torch.float32)
+            ids = torch.full((len(t), 1), t_id, dtype=torch.float32)
+
+            result.append(torch.cat([points, ids], dim=1))
+
+        return torch.cat(result)
+
+    @staticmethod
+    def _normalise_data(data: torch.Tensor) -> torch.Tensor:
+        """Perform normalization on data tensor
+        """
+        xy = data[:, :2]
+        xy_min = xy.min(dim=0, keepdim=True).values
+        xy_max = xy.max(dim=0, keepdim=True).values
+        t0 = data[:, 2].min(dim=0, keepdim=True).values
+
+        data[:, :2] = (xy - xy_min) / (xy_max - xy_min + HMEDataset.EPS)
+        data[:, 2] -= t0
+
+        return data
+
 
 
 if __name__ == "__main__":
     d = HMEDataset('data/train')
-    for x in d:
-        print(x)
+    ink = d[4]
+    HMEDataset.extract_features(ink)
