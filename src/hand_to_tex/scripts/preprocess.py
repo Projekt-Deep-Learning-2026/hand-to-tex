787a267e0ee43a91e0807d0b06c539bd8d0084c5
LKRenjoyer <m.ovczarz@gmail.com>
2026-03-29 15:50:53 +0200

    Feat, added options to preprocess script, out_dir, start_idx, capacity

--- src/hand_to_tex/scripts/preprocess.py
+++ src/hand_to_tex/scripts/preprocess.py
@@ -1,6 +1,7 @@
 import argparse
 import concurrent.futures
 import itertools
+import os
 from pathlib import Path
 from typing import Literal
 
@@ -39,19 +40,34 @@ def _process_single_file(
         return "error", None
 
 
-def preprocess_split(root: Path, split_name: str, vocab: LatexVocab, num_workers: int):
+def preprocess_split(
+    root: Path,
+    out_dir: Path,
+    split_name: str,
+    vocab: LatexVocab,
+    num_workers: int,
+    start_idx: int,
+    capacity: int | None,
+):
     split_dir = root / split_name
     if not split_dir.exists():
         print(f"Directory {split_dir} not found")
         return
+    if not out_dir.exists():
+        print(f"Output directory {out_dir} not found, creating")
+        os.mkdir(out_dir)
 
     inkmls = sorted(split_dir.rglob("*.inkml"))
-    if not inkmls:
-        print(f"No .inkml files found in {split_dir}")
+    if len(inkmls) <= start_idx:
+        print(f"No .inkml files found in {split_dir} (after skipping {start_idx} files)")
         return
+    inkmls = inkmls[start_idx:]
+
+    if capacity:
+        inkmls = inkmls[:capacity]
 
     print(
-        f"Processing split {split_name}, {len(inkmls)} .inkml files found using {num_workers} workers"
+        f"Processing split {split_name}, {len(inkmls)} .inkml files found, using {num_workers} workers"
     )
 
     temp_files = []
@@ -78,12 +94,12 @@ def preprocess_split(root: Path, split_name: str, vocab: LatexVocab, num_workers
                             err_cnt += 1
                     pbar.update(1)
 
-                temp_path = root / f"{split_name}_temp_{b_idx}.pt"
+                temp_path = out_dir / f"{split_name}_temp_{b_idx}.pt"
                 torch.save(processed, temp_path)
                 temp_files.append(temp_path)
                 processed = []
 
-        out_file_path = Path(root, split_name + ".pt")
+        out_file_path = Path(out_dir, split_name + ".pt")
         final_data = []
 
         print(f"Saving to {out_file_path}")
@@ -123,24 +139,49 @@ def main():
     parser.add_argument(
         "--threads",
         type=int,
+        required=False,
         default=1,
         help="Number of workers (processes) to use for preprocessing, default=1",
     )
     parser.add_argument(
         "--splits",
         nargs="+",
+        required=False,
         choices=SPLITS,
         default=SPLITS,
         help=f"List of splits to preprocess, choices are: {', '.join(SPLITS)}, by default all splits will be preprocessed",
     )
+    parser.add_argument(
+        "--out_dir",
+        type=str,
+        required=False,
+        help="Directory where .pt preprocessed splits are going to be saved, by default the `root` directory will be used",
+    )
+    parser.add_argument(
+        "--start_idx",
+        type=int,
+        default=0,
+        required=False,
+        help="While preprocessing splits first `start_idx` files will be omitted, helpful for generating partial datasets",
+    )
+    parser.add_argument(
+        "--capacity",
+        type=int,
+        required=False,
+        help="At most `capacity` files are going to be saved, helpful for creating partial datasets",
+    )
     args = parser.parse_args()
+
+    if args.capacity is not None and args.capacity <= 0:
+        parser.error("Argument --capacity, when passed, must be > 0")
 
     root = Path(args.root)
     vocab = LatexVocab.load(args.vocab)
+    out_dir = root if args.out_dir is None else Path(args.out_dir)
 
     print(f"Preprocessing splits: {', '.join(args.splits)}")
     for split in args.splits:
-        preprocess_split(root, split, vocab, args.threads)
+        preprocess_split(root, out_dir, split, vocab, args.threads, args.start_idx, args.capacity)
 
 
 if __name__ == "__main__":
