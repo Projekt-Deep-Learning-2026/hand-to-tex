d91fb083852220fb3c29a2da0efe0a30facdeb9c
LKRenjoyer <m.ovczarz@gmail.com>
2026-04-24 17:40:52 +0200

    Add, types for dataset file

--- src/hand_to_tex/types.py
+++ src/hand_to_tex/types.py
@@ -1,4 +1,29 @@
+from collections.abc import Callable
+
+from torch import Tensor
+
 # Ink Data types
 type TracePoint = tuple[float, float, float]
 type Trace = list[TracePoint]
 type Traces = list[Trace]
+
+# Tensor types
+type TensorF32 = Tensor
+"""Tensor with `dtype=torch.float32`"""
+type TensorF16 = Tensor
+type TensorI16 = Tensor
+type TensorI32 = Tensor
+type TensorLong = Tensor
+type TensorBool = Tensor
+
+type Transformation[TensorType: Tensor] = Callable[[TensorType], TensorType]
+"""Transforms given tensor into another one"""
+
+type Features = TensorF32
+"""Feature tensor"""
+
+type Tokens = TensorLong
+"""Token tensor"""
+
+type Sample = tuple[Features, Tokens]
+"""Single sample [fts, tokens]"""
