2c68851737836c10e7a61506600857f2384add4e
LKRenjoyer <m.ovczarz@gmail.com>
2026-05-11 19:56:55 +0200

    Add, onnx-export contracts

--- src/hand_to_tex/models/components/exportable.py
+++ src/hand_to_tex/models/components/exportable.py
@@ -1,10 +1,27 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from collections.abc import Callable
+from dataclasses import dataclass
 
-from hand_to_tex.models.components.base import BaseDecoderModel
+
+@dataclass
+class OnnxExportConfiguration:
+    """Component's configuration describing script format for onnx-export"""
+
+    name: str
+    export_fun: Callable
+
+    dummy_inputs: tuple
+    input_names: list[str]
+    output_names: list[str]
+    dynamic_axes: dict[str, dict[int, str]]
 
 
-class OnnxExportable(BaseDecoderModel, ABC):
+class OnnxExportable(ABC):
     """
     Describes an interface for onnx-exporting script to make the nn.Module totally abstract
     from script logic
     """
+
+    @abstractmethod
+    def get_onnx_export_configs(self) -> list[OnnxExportConfiguration]:
+        raise NotImplementedError
