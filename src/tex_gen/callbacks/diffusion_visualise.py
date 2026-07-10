1ac0b6fdcae897e8c0ae591c9501265ea3fc145b
LKRenjoyer <m.ovczarz@gmail.com>
2026-07-10 14:06:16 +0200

    Add, visualisation callback

--- src/tex_gen/callbacks/diffusion_visualise.py
+++ src/tex_gen/callbacks/diffusion_visualise.py
@@ -1,7 +1,10 @@
 import lightning.pytorch as pl
+import matplotlib.pyplot as plt
 import torch
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 
-from tex_gen.datasets.utils import NUM_FEATURES
+from tex_gen.datasets.utils import NUM_FEATURES, draw_features
 from tex_gen.types import Features
 
 
@@ -19,6 +22,29 @@ class DiffusionVisualisationCallback(pl.Callback):
         self.every_n_epochs = every_n_epochs
         self.image_count = image_count
         self.show_steps = sorted(show_steps, reverse=True)
+
+    @staticmethod
+    def _display_samples(states: dict[int, list[Features]], num_images: int, epoch: int) -> Figure:
+        fig, axs = plt.subplots(
+            ncols=len(states),
+            nrows=num_images,
+            figsize=(4 * len(states), 4 * num_images),
+            squeeze=False,
+        )
+
+        for x_ax, stamp in enumerate(sorted(states.keys(), reverse=True)):
+            images = states[stamp]
+            for y_ax, img in enumerate(images):
+                ax: Axes = axs[y_ax, x_ax]
+                draw_features(fts=img, ax=axs[y_ax, x_ax])
+
+                if y_ax == 0:
+                    ax.set_title(f"Step $t={stamp}$")
+
+        fig.suptitle(f"Generation | epoch={epoch}", fontsize=16)
+        fig.tight_layout()
+
+        return fig
 
     @torch.inference_mode()
     def on_train_epoch_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
@@ -43,8 +69,8 @@ class DiffusionVisualisationCallback(pl.Callback):
 
         x = x * mask.unsqueeze(-1).float()
 
-        states: dict[int, Features] = {}
-        for t in pl_module.noise_scheduler.timestamps:  # type: ignore
+        states: dict[int, list[Features]] = {}
+        for t in pl_module.noise_scheduler.timesteps:  # type: ignore
             t_tensor = torch.tensor([t], device=pl_module.device)
 
             residual = pl_module.model(x, t_tensor, src_key_padding_mask=~mask)  # type: ignore
@@ -54,4 +80,14 @@ class DiffusionVisualisationCallback(pl.Callback):
 
             t_val = t.item()
             if t_val in self.show_steps:
-                states[t_val] = x.detach().cpu().clone()
+                cpu_x = x.detach().cpu().clone()
+                fts = [cpu_x[i, : lengths[i].item()] for i in range(self.image_count)]
+
+                states[t_val] = fts
+
+        _fig = self._display_samples(
+            states=states, num_images=self.image_count, epoch=trainer.current_epoch
+        )
+        plt.show()
+
+        pl_module.train()
